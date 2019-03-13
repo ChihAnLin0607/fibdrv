f454268851036c2073994981aa7b008df025b3fa
ChihAnLin0607 <qsyyzxp12@gmail.com>
2019-03-13 12:39:25 +0800

    Implement multi function with BigN

--- bigN.h
+++ bigN.h
@@ -20,10 +20,41 @@ static inline void printBigN(struct BigN n)
 #endif
 }
 
+static inline void shift_l_BigN(struct BigN *output, struct BigN x)
+{
+    output->upper = (x.upper << 4) + (x.lower >> 60);
+    output->lower = x.lower << 4;
+}
+
 static inline void addBigN(struct BigN *output, struct BigN x, struct BigN y)
 {
     output->upper = x.upper + y.upper;
     if (y.lower > ~x.lower)
         output->upper++;
     output->lower = x.lower + y.lower;
+}
+
+static inline short getDigit(struct BigN x, short i)
+{
+    if (i < 15)
+        return (x.lower >> i * 4) & 0xf;
+    else
+        return (x.upper >> (i - 16) * 4) & 0xf;
+}
+
+static inline void multiBigN(struct BigN *output, struct BigN x, struct BigN y)
+{
+    int i = 0, j = 0;
+    short digit;
+    output->lower = 0;
+    output->upper = 0;
+
+    for (i = 0; i < 32; i++) {
+        if ((digit = getDigit(y, i))) {
+            j = 0;
+            while (j++ < digit)
+                addBigN(output, *output, x);
+        }
+        shift_l_BigN(&x, x);
+    }
 }
